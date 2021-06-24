9c47c49f33b93114605606f15c437a4c1ef8d8b9
riyadhrazzaq <riyadh.razzaq@gmail.com>
2021-06-24 15:35:23 +0600

    used csv module to parse csv files

--- src/sqlcsvsql/csv_to_sql.py
+++ src/sqlcsvsql/csv_to_sql.py
@@ -1,21 +1,53 @@
-import pandas as pd
+import csv
 
 config = {}
 output = {}
 
 
 def run(table_name, file_name, is_multiline=False):
+    """
+    driver method.
+
+    Parameters
+    ----------
+    table_name: str
+        name of the table to use in DML
+    file_name: str
+        input csv file
+    is_multiline: bool
+        whether to generate multiple insert statements or single one.
+
+    Returns
+    -------
+    Nothing. Saves the file on disk
+    """
     config["table"] = table_name
 
-    table = pd.read_csv(file_name)
-    config["columns"] = list(table.columns.values)
+    table = read_csv(file_name)
+    config["columns"] = list(table[0].keys())
     config["is_multiline"] = is_multiline
     output[config["table"]] = {"values": [], "insert_into": ""}
     write_to_sql(file_name + ".sql", prepare_sql(table))
 
 
+def read_csv(file_name):
+    """
+    read csv
+
+    Returns
+    -------
+    rows: List[Dict]
+        list of dict. each dict contains csv in {key:value} format
+    """
+    rows = []
+    with open(file_name) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            rows.append(row)
+    return rows
+
+
 def write_to_sql(file_name, content):
-    print(output[config["table"]]["values"])
     with open(file_name, "w") as f:
         f.write(content)
 
@@ -35,14 +67,18 @@ def prepare_values(line):
     converts line in a csv to sql
     """
     table_name = config["table"]
-    values = list(line)
+    values = []
+    for k in config["columns"]:
+        values.append(line[k])
     statement = prepare_statement(values)
     output[table_name]["values"].append(statement)
 
 
 def prepare_sql(table):
+    """Generates the sql given a csv table"""
     sql = ""
-    table.apply(prepare_values, axis=1)
+    for row in table:
+        prepare_values(row)
     insert_statement = prepare_insert_header_statement(
         config["table"], config["columns"]
     )
@@ -53,5 +89,4 @@ def prepare_sql(table):
 
     for v in output[config["table"]]["values"]:
         sql = sql + "\n" + insert_statement + " " + v + ";"
-    print(sql)
     return sql
